8eedc2ceb828dc87a808b2c23e5121e595d8323e
Dean Phommahaxay <deanp21@gmail.com>
2024-03-16 21:14:53 +1300

    Count characters with respect to locale

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <ctype.h>
+#include <locale.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wchar.h>
 
 struct options {
   bool count_bytes;
   bool count_lines;
   bool count_words;
+  bool count_chars;
 };
 
 typedef struct options Options;
 
 bool nooptsset(Options options) {
-  return !options.count_bytes && !options.count_lines && !options.count_words;
+  return !options.count_bytes && !options.count_lines && !options.count_words &&
+         !options.count_chars;
 }
 
-void setallopts(Options *options) {
+void setdefaultopts(Options *options) {
   options->count_bytes = true;
   options->count_lines = true;
   options->count_words = true;
@@ -46,7 +50,7 @@ unsigned long countlines(FILE *fp) {
     exit(EXIT_FAILURE);
   }
 
-  int lines = 0;
+  long lines = 0;
   char ch;
 
   while ((ch = fgetc(fp)) != EOF) {
@@ -65,7 +69,7 @@ unsigned long countwords(FILE *fp) {
     exit(EXIT_FAILURE);
   }
 
-  int words = 0;
+  long words = 0;
   char ch;
   bool in_word;
 
@@ -83,11 +87,31 @@ unsigned long countwords(FILE *fp) {
   return words;
 }
 
+unsigned long countchars(FILE *fp) {
+  if (fseek(fp, 0, SEEK_SET) || ferror(fp)) {
+    perror("fseek");
+    fclose(fp);
+    exit(EXIT_FAILURE);
+  }
+
+  // Use current environment locale's character encoding
+  setlocale(LC_CTYPE, "");
+
+  unsigned long chars = 0;
+  wchar_t ch;
+
+  while ((ch = fgetwc(fp)) != WEOF) {
+    chars++;
+  }
+
+  return chars;
+}
+
 int main(int argc, char *argv[]) {
   int opt;
   Options options = {false, false, false};
 
-  while ((opt = getopt(argc, argv, "clw")) != -1) {
+  while ((opt = getopt(argc, argv, "lwcm")) != -1) {
     switch (opt) {
     case 'c':
       options.count_bytes = true;
@@ -98,6 +122,8 @@ int main(int argc, char *argv[]) {
     case 'w':
       options.count_words = true;
       break;
+    case 'm':
+      options.count_chars = true;
     }
   }
 
@@ -106,9 +132,8 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  // Default behaviour is to set on all options (when all are not set)
   if (nooptsset(options)) {
-    setallopts(&options);
+    setdefaultopts(&options);
   }
 
   char *filename = argv[optind];
@@ -118,19 +143,21 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  if (options.count_bytes) {
-    printf("%li ", countbytes(fp));
-  }
-
   if (options.count_lines) {
     printf("%li ", countlines(fp));
   }
-
   if (options.count_words) {
     printf("%li ", countwords(fp));
   }
+  if (options.count_bytes) {
+    printf("%li ", countbytes(fp));
+  }
+  if (options.count_chars) {
+    printf("%li ", countchars(fp));
+  }
 
   printf("%s\n", filename);
+
   fclose(fp);
 
   return 0;
